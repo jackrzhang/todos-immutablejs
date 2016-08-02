3b74f7928f85b53b7d83456b4194377bb80dea83
Jack Zhang <15jzhang@gmail.com>
2016-08-01 21:36:39 -0400

    Remove specs for react components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import styles from './App.css';
 
-import Subtitle from './Subtitle';
-
 // react-hot-loader prior to version 3 does not
 // support stateless functional root components
 class App extends Component {
@@ -13,7 +11,6 @@ class App extends Component {
     return (
       <div className={styles.app}>
         <h1 className={styles.title}>React Webpack Boilerplate</h1>
-        <Subtitle />
       </div>
     );
   }
