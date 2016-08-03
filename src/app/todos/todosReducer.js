f48f5bc9a609a7ec9ca3c3ff7b83c4d0afa419e1
Jack Zhang <15jzhang@gmail.com>
2016-08-02 23:58:34 -0400

    Implement todos reducer case for clear completed action

--- src/app/todos/todosReducer.js
+++ src/app/todos/todosReducer.js
@@ -1,5 +1,6 @@
-import { ADD_TODO } from './../input/inputActions.js';
-import { COMPLETE_TODO } from './../todos/todosActions.js';
+import { ADD_TODO } from './../input/inputActions';
+import { COMPLETE_TODO } from './../todos/todosActions';
+import { CLEAR_COMPLETED } from './../footer/footerActions';
 
 const initialTodos = [
   {
@@ -42,39 +43,11 @@ const todosReducer = (state = initialTodos, action) => {
           Object.assign({}, todo, { complete: !todo.complete }) :
           todo
       );
+    case CLEAR_COMPLETED:
+      return state.filter(todo => !todo.complete);
     default:
       return state;
   }
 };
 
 export default todosReducer;
-
-// const friendsListReducer = (state = initalFriendsList, action) => {
-//   switch (action.type) {
-//     case types.ADD_FRIEND:
-//       return [
-//         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-//         {
-//           id: state.reduce((maxId, friend) => Math.max(friend.id, maxId), -1) + 1,
-//           starred: false,
-//           name: action.name
-//         },
-//         ...state
-//       ];
-//     case types.REMOVE_FRIEND:
-//       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-//       return state.filter(friend => friend.id !== action.id);
-//     case types.STAR_FRIEND:
-//       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-//       return state.map(friend =>
-//         friend.id === action.id ?
-//           Object.assign({}, friend, { starred: !friend.starred }) :
-//           friend
-//       );
-//     default:
-//       return state;
-//   }
-// };
-
-// export default friendsListReducer;
-
