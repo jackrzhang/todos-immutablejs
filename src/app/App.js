481d4a02628aa69b7fde2179d49267004a523aa2
Jack Zhang <15jzhang@gmail.com>
2016-08-02 20:17:07 -0400

    Add credit element with styling

--- src/app/App.js
+++ src/app/App.js
@@ -10,6 +10,12 @@ class App extends Component {
     return (
       <div>
         <h1 className={styles.title}>todos</h1>
+        <div className={styles.list}>
+
+        </div>
+        <h6 className={styles.credit}>made with &#9825; by&nbsp;
+          <a target="_blank" href="https://github.com/jackrzhang">jackrzhang</a>
+        </h6>
       </div>
     );
   }
